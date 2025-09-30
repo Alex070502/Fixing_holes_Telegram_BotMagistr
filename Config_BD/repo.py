c783dace5ea57ec8d23efedf59b416d8c75cd8e2
Alex070502 <116755484+Alex070502@users.noreply.github.com>
2025-09-30 11:15:23 +0300

    delete print

--- Config_BD/repo.py
+++ Config_BD/repo.py
@@ -8,7 +8,6 @@ from Config_BD.Basemodul import engine, wa, pa, users
 
 class SQL:
     session = Session(engine)
-    print("alex loh")
     def well_app_insert(self,
                         address: str, longtitude: float, latitude: float,
                         user_id: int, photo: bytearray, det_photo: bytearray, date: datetime):
